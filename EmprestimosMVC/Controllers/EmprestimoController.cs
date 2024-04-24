e4c5238031af95520c7ce22d1b7501ab707384d5
Vitor <vitorlzampronha@gmail.com>
2024-04-24 17:32:48 -0300

    criacao view cadastrar

--- EmprestimosMVC/Controllers/EmprestimoController.cs
+++ EmprestimosMVC/Controllers/EmprestimoController.cs
@@ -17,9 +17,28 @@ namespace EmprestimosMVC.Controllers
         public IActionResult Index()
         {
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
-
-
             return View(emprestimos);
         }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
     }
 }
